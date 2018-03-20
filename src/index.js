81be2b7a6be29c4486b9ce5784a6792ec699936f
jiashunming <jiashunming@outlook.com>
2018-03-20 21:45:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-// @flow
-function say (str: string): string | number {
-  console.log(str)
+/* @flow */
+function say(str: string): string | number {
+    console.log(str)
 
-  return 1234
+    return 1234
 }
 
 let result = say('123')
 
 console.log(result)
 
-function createEle (tag: string): HTMLElement {
-  return document.createElement(tag)
+function createEle(tag: string): HTMLElement {
+    return document.createElement(tag)
 }
 
-function append (node: HTMLElement) {
-  let tag = document.querySelector('body')
-  if (tag) {
-    tag.appendChild(node)
-  }
+function append(node: HTMLElement) {
+    let tag = document.querySelector('body')
+    if (tag) {
+        tag.appendChild(node)
+    }
 }
 
 let $wrap = createEle('div')
@@ -29,9 +29,9 @@ $wrap.appendChild($title)
 append($wrap)
 
 class Test {
-  log (msg: string | number) {
-    console.log(msg)
-  }
+    log(msg: string | number) {
+        console.log(msg)
+    }
 }
 
 let test = new Test()
